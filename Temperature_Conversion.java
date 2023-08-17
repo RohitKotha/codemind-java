694a6887b267be8894e6474ddc27bd00c279a58f
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-08-17 09:45:30 +0530

    import java.util.*; public class rohit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         System.out.printf("%.2f",32+(a*9/5));     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class rohit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        System.out.printf("%.2f",32+(a*9/5));
+    }
+}
