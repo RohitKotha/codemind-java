980acf69ec2f87a05992024d452beab4a7362fb0
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-08-19 10:19:07 +0530

    import java.util.*; public class rohit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=sc.nextInt();         int b=sc.nextInt();         System.out.println(n*s*b*1024);         }     }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class rohit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(n*s*b*1024);
+        }
+    }
