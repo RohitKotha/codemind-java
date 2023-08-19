83f2e26c82fbcb881a179436b2d76a7bfab92740
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-08-19 10:02:46 +0530

    import java.util.*; public class rohit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double p=3.14*(a*a);         System.out.printf("%.2f",p);         }     }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class rohit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double p=3.14*(a*a);
+        System.out.printf("%.2f",p);
+        }
+    }
