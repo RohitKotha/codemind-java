6cbc680364868a930d1af201fbd760bed764ffeb
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-08-19 10:24:33 +0530

    import java.util.*; public class rohit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String st=sc.next();         int d=Integer.parseInt(st,8);         System.out.println(d);         }     }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class rohit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String st=sc.next();
+        int d=Integer.parseInt(st,8);
+        System.out.println(d);
+        }
+    }
