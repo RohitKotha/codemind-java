b1b1571f525cb0270c3dc6bbe4dde68f30b1bd3d
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-09-11 15:45:20 +0530

    import java.util.*; public class hero{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         if(n==1){             System.out.print(arr[n-1]);         }         else{             n=n-1;             while(true){             if(arr[n]%2!=0){                n--;             }             else if(arr[n]%2==0){                 System.out.print(arr[n]);                 break;             }         }         }     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class hero{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        if(n==1){
+            System.out.print(arr[n-1]);
+        }
+        else{
+            n=n-1;
+            while(true){
+            if(arr[n]%2!=0){
+               n--; 
+            }
+            else if(arr[n]%2==0){
+                System.out.print(arr[n]);
+                break;
+            }
+        }
+        }
+    }
+}
